43f2c1f1bbe39b8b15093711bbe5de535bcb2c86
AllanTumu <allantumu1962@gmail.com>
2022-03-29 16:57:50 +0300

    Worked on Objects

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-function add(n1, n2, showResult, phrase) {
-    console.log(typeof number1);
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-//Data types in TypeScript
-// type number
-var number1 = 5;
-var number2 = 2.8;
-// type boolean
-var printResult = true;
-// type string
-var phrase = 'Result is ';
-add(number1, number2, printResult, phrase);
+var user = {
+    name: 'Allan',
+    age: 40,
+    profession: 'QA Engineer'
+};
+console.log(user);
--- app.ts
+++ app.ts
@@ -1,21 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    console.log(typeof number1)
-
-    const result = n1+n2;
-    if (showResult) {
-        console.log(phrase + result)
-    } else {
-        return result;
-    }
+const user = {
+    name: 'Allan',
+    age: 40,
+    profession: 'QA Engineer'
 }
-
-//Data types in TypeScript
-// type number
-const number1 = 5;
-const number2 = 2.8;
-// type boolean
-const printResult = true;
-// type string
-const phrase = 'Result is ';
-
-add(number1, number2, printResult, phrase);
+console.log(user);
